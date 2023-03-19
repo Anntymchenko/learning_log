33ee1d9f33e91399fb50a7b01ed37a6a252ca67c
Anntymchenko <124369819+Anntymchenko@users.noreply.github.com>
2023-03-19 22:42:04 +0200

    Users

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LearningLogsConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'learning_logs'
+    name = 'users'
